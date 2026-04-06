d8efd8a95c51ca7a8abe9d6690c5de5b861124f2
Ben O'Neill <ben@oneill.sh>
2026-04-06 18:01:42 -0400

    Compress locally stored images

--- src/helpers/image_cache.py
+++ src/helpers/image_cache.py
@@ -1,9 +1,9 @@
 """
 Image cache helper module.
 """
-import os, urllib.request
-from urllib.parse import urlparse
+import os, requests
 from flask import request
+from PIL import Image
 
 IMAGE_CACHE_DIR = os.path.join(os.path.dirname(__file__), "..", "static", "img")
 
@@ -18,9 +18,19 @@ def get_image_path(image_url: str) -> str:
         os.makedirs(IMAGE_CACHE_DIR)
 
     url_hash = hash(image_url)
+    target_path = f"{IMAGE_CACHE_DIR}/{url_hash}"
 
-    if not os.path.exists(os.path.join(IMAGE_CACHE_DIR, f"{url_hash}")):
-        with open(f"{IMAGE_CACHE_DIR}/{url_hash}", "wb") as f:
-            f.write(urllib.request.urlopen(image_url).read())
+    if not os.path.exists(target_path):
+        with open(target_path, "wb") as f:
+            response = requests.get(image_url, stream=True)
+            for block in response.iter_content(1024):
+                    if not block:
+                        break
+                    f.write(block)
+        with Image.open(target_path) as img:
+            rgb_img = img.convert("RGB")
+            resized_img = rgb_img.resize((128, 128))
+            resized_img.save(target_path, "JPEG", optimize=True, quality=70)
+
 
     return f"{base}/{url_hash}"
