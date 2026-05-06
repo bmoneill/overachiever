020c55ddbaddfd4421b009bed2ffdf2d902093d7
Ben O'Neill <ben@oneill.sh>
2026-05-06 12:45:04 -0400

    start.sh: Load venv

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ GUNICORN_WORKERS=2
 GUNICORN_THREADS=4
 export COMPRESSOR_WORKERS=4
 
+source .venv/bin/activate
 python scripts/image_compressor.py &
-gunicorn --bind 0.0.0.0:5000 --workers 2 --threads 4 --worker-class gthread src:app
+gunicorn --bind 0.0.0.0:8001 --workers 2 --threads 4 --worker-class gthread src:app
